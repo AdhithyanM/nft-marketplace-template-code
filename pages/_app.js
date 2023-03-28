cef10eacd28fcfaaddc05ba80c808e30df411fa5
AdhithyanM <adhithyanmanoharan26@gmail.com>
2023-03-28 23:38:16 +0530

    Footer Component Completion

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import "../styles/globals.css";
 
 // INTERNAL IMPORT
-import { NavBar } from "../components/components-index";
+import { Footer, NavBar } from "../components/components-index";
 
 const MyApp = ({ Component, pageProps }) => (
   <div>
     <NavBar />
     <Component {...pageProps} />
+    <Footer />
   </div>
 );
 
