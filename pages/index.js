e7908a5cf1d465673dc319f83cc3b1702e0e849e
AdhithyanM <adhithyanmanoharan26@gmail.com>
2023-03-30 21:16:48 +0530

    BigNFTSlider Component in the Home page

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,18 @@ import React from "react";
 
 // INTERNAL IMPORT
 import Style from "../styles/index.module.css";
-import { HeroSection, Service } from "../components/components-index";
+import {
+  HeroSection,
+  Service,
+  BigNFTSlider,
+} from "../components/components-index";
 
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection />
       <Service />
+      <BigNFTSlider />
     </div>
   );
 };
